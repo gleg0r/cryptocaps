d7fe374a3447a3edd80e31e111a2911ecec8bc86
gleg0r <egor103230@gmail.com>
2023-11-23 14:21:12 +0300

    add custom dropdown

--- src/Components/header/dropdown.jsx
+++ src/Components/header/dropdown.jsx
@@ -4,11 +4,23 @@ import style from "./dropdown.module.scss";
 function DropDown() {
 
     return(
-        <div>
-            <select onfocus='this.size=10;' onblur='this.size=0;' onchange='this.size=1; this.blur();' className={style.dropdown}>
-                <option   value="russian">Рус</option>
-                <option   value="english">Eng</option>
-            </select>
+        <div className={style.DropDown}>
+            <p id="selectedLang" className={style.DropDown__selected}>Рус</p>
+            <ul id="dropdown" className={style.DropDown__list}>
+                <li className={style.DropDown__item}
+                    style={{borderRadius: "2px 2px 0px 0px"}}
+                >
+                    Рус
+                </li>
+                <li className={style.DropDown__item}
+                     style={{borderRadius: "0px 0px 2px 2px"}}
+                >
+                    En
+                </li>
+            </ul>
+            <svg className={style.DropDown__arrow} xmlns="http://www.w3.org/2000/svg" width="12" height="6" viewBox="0 0 12 6" fill="none">
+                <path d="M6 6L0.803848 0L11.1962 0L6 6Z" fill="#FDFAFF"/>
+            </svg>  
         </div>
     )
 }
