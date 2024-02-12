30475770b39446e202f276acb59f15137aaab770
gleg0r <egor103230@gmail.com>
2024-02-12 16:13:09 +0300

    prod build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.scss';
-import Video from './Components/pages/video';
 import Home from './Components/pages/home';
 import Quests from './Components/pages/quests';
 import Rating from './Components/pages/rating';
